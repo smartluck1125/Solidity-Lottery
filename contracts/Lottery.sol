fe0ab8d61f43807c9d2fad8bb291ac72eafc2f1b
smartluck1125 <onemoretimeforu@outlook.com>
2020-11-20 11:05:06 +0100

    Added the pseudo random function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,6 +1,9 @@
-pragma solidity ^0.7.0;
+pragma solidity >=0.5.15 <0.8.0;
 // SPDX-License-Identifier: UNLICENCED
 
 contract Lottery {
-
+    function getRandomNumber() private view returns (uint) {
+        uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+        return (randomHash % 1000) + 1;
+    } 
 }
