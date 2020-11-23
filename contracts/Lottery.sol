c8dc354e35b68722fe7ae201ae0814306f25a1a0
smartluck1125 <onemoretimeforu@outlook.com>
2020-11-23 19:05:53 +0100

    Added the Ownable contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,9 @@
-pragma solidity >=0.5.15 <0.8.0;
 // SPDX-License-Identifier: UNLICENCED
+pragma solidity >=0.5.15 <0.8.0;
 
-contract Lottery {
+import './Ownable.sol';
+
+contract Lottery is Ownable {
     function getRandomNumber() private view returns (uint) {
         uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
         return (randomHash % 1000) + 1;
