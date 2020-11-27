edaf084965456f62b6df628c99b40e8e6e45159f
smartluck1125 <onemoretimeforu@outlook.com>
2020-11-27 17:22:13 +0100

    Added functionality to add or remove participants

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,6 +10,9 @@ contract Lottery is Ownable {
     uint roundsPlayed = 0;
     uint cooldownTime = 4 weeks;
     uint bettingAmount = 0.1 ether;
+    uint8 amountParticipants = 0; // The group consists of 5 friends, but who knows maybe they want to add more (up to 255)
+
+    mapping(address => bool) private _isLotteryParticipant;
 
     //////////////////////////////////////
     ///        Public functions        ///
@@ -17,13 +20,13 @@ contract Lottery is Ownable {
 
     /// @notice Get the current round number
     /// @dev We return + 1 since roundsPlayed keeps track of the amount of rounds that have been played (and finished)
-    /// @return the current lottery round
+    /// @return The current lottery round
     function getCurrentRound() public view returns (uint) {
         return roundsPlayed + 1;
     }
 
     /// @notice Get the current pot balance
-    /// @return the current pot balance in Wei
+    /// @return The current pot balance in Wei
     function getPotBalance() public view returns (uint) {
         return address(this).balance;
     }
@@ -38,12 +41,28 @@ contract Lottery is Ownable {
         bettingAmount = _amount;
     }
 
+    /// @notice Add a participant address to the lottery (Owner only)
+    /// @param _participant The address of the participant to add to the lottery
+    function addParticipant(address _participant) external onlyOwner {
+        require(_isLotteryParticipant[_participant] == false, "This address is already a participant");
+        _isLotteryParticipant[_participant] = true;
+        amountParticipants++;
+    }
+
+    /// @notice Remove a participant address to the lottery (Owner only)
+    /// @param _participant The address of the participant to remove from the lottery
+    function removeParticipant(address _participant) external onlyOwner {
+        require(_isLotteryParticipant[_participant] == true, "This address is not a known participant");
+        _isLotteryParticipant[_participant] = false;
+        amountParticipants--;
+    }
+
     //////////////////////////////////////
     ///  Internal & private functions  ///
     //////////////////////////////////////
 
     /// @notice Returns a random(ish) number between 1-1000 (including 1 and 1000)
-    /// @return a uint between 1 and 1000 (including 1 and 1000)
+    /// @return A uint between 1 and 1000 (including 1 and 1000)
     function getRandomNumber() internal returns (uint) {
         randNonce++;
         return (uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % 1000) + 1;
