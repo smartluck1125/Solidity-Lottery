4d61981e62d381deac36f709ee2c7b182cbf324a
smartluck1125 <onemoretimeforu@outlook.com>
2020-11-29 17:33:06 +0100

    Added empty solidity test file

---
+++ test/TestLottery.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENCED
+pragma solidity >=0.5.15 <0.8.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Lottery.sol";
+
+contract TestLottery {
+
+}
