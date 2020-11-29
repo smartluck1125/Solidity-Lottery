83624efc01654dcd2bb8b9dbc517a6b79df63b77
smartluck1125 <onemoretimeforu@outlook.com>
2020-11-29 16:20:36 +0100

    Added events and functions to draw a winner/ start new round

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,6 +8,7 @@ import './Ownable.sol';
 contract Lottery is Ownable {
     uint randNonce = 0;
     uint roundsPlayed = 0;
+    uint lastLotteryDraw = 0;
     uint cooldownTime = 4 weeks;
     uint bettingAmount = 0.1 ether;
     uint8 amountParticipants = 0; // The group consists of 5 friends, but who knows maybe they want to add more (up to 255)
@@ -19,6 +20,13 @@ contract Lottery is Ownable {
 
     mapping(address => bool) private _isLotteryParticipant;
     BettingEntry[] private bettingEntries;
+    
+    //////////////////////////////////////
+    ///             Events             ///
+    //////////////////////////////////////
+    event NewRoundStarted(uint roundNumber);
+    event RoundWinner(address winner, uint amount);
+    event NoWinners();
 
     //////////////////////////////////////
     ///        Public functions        ///
@@ -69,6 +77,43 @@ contract Lottery is Ownable {
         amountParticipants--;
     }
 
+    /// @notice Draw a winner
+    /// @dev Not sure if it is bettingEntries.length or with a -1 need to test
+    function drawWinner() external onlyOwner {
+        // Check if a month has been passed since the last draw
+        require(lastLotteryDraw + cooldownTime <= block.timestamp, "The lottery can only be executed once a month");
+        // Check if every participant has placed a bet
+        require(bettingEntries.length == amountParticipants, "Not every participant has placed a bet yet");
+        // Generate a random(ish) number
+        uint winningNumber = getRandomNumber();
+        // Create an array to push the winners into
+        address[] storage winners;
+        // Loop over the bettingEntries to check if anyone has won
+        for (uint i = 0; i < bettingEntries.length; i++) {
+            // If someone has won push the address onto the winners array
+            if (bettingEntries[i].number == winningNumber) {
+                winners.push(bettingEntries[i].participant);
+            }
+        }
+        // Are there any winners?
+        if (winners.length > 0) {
+            // Calculate the prize that every winner receives
+            uint prize = address(this).balance / winners.length;
+            // Loop through the winners, transfer the prize and emit a event
+            for (uint i = 0; i < winners.length; i++) {
+                payable(winners[i]).transfer(prize);
+                emit RoundWinner(winners[i], prize);
+            }
+        } else {
+            // Emit event that no winners are drawn
+            emit NoWinners();
+        }
+        // Start a new round
+        startNewRound();
+        // TODO: Emit event to notify that a new round has started
+        emit NewRoundStarted(getCurrentRound());
+    }
+
     //////////////////////////////////////
     ///  Internal & private functions  ///
     //////////////////////////////////////
@@ -88,12 +133,24 @@ contract Lottery is Ownable {
     /// @notice Check if an given address already has an existing bet placed
     /// @return true if an existing bet is placed, false if not
     function hasExistingBet(address _participant) private view returns (bool) {
-        for(uint i = 0; i < bettingEntries.length; i++) {
+        for (uint i = 0; i < bettingEntries.length; i++) {
             if (bettingEntries[i].participant == _participant) {
                 return true;
             }
         }
         return false;
+    }
+
+    /// @notice Start the lottery for a new round
+    /// @dev This is a pretty dangerous function since it doesn't do any checks
+    /// for example: checking if it has been a month since last "reset"
+    function startNewRound() private {
+        // Increase the rounds played counter
+        roundsPlayed++;
+        // Clear the previous betting entries inside the array
+        delete bettingEntries;
+        // Set the last lottery draw to the current block timestamp
+        lastLotteryDraw = block.timestamp;
     }
 
     /// @notice Returns a random(ish) number between 1-1000 (including 1 and 1000)
